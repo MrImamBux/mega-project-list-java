450647364e7bbacc2a2de4755758fb15ef4d6216
Imam Bux <imam.bux@idealo.de>
2020-02-29 12:19:22 +0100

    Events for Edit Menu

--- text-editor/src/main/java/com/imambux/ParentController.java
+++ text-editor/src/main/java/com/imambux/ParentController.java
@@ -3,8 +3,12 @@ package com.imambux;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.HBox;
 
 public class ParentController implements Initializable {
@@ -12,11 +16,59 @@ public class ParentController implements Initializable {
     public MenuItem showStatusBar;
     public HBox statusBar;
 
+    public TextArea textArea;
+
+    public MenuItem editCut;
+    public MenuItem editCopy;
+    public MenuItem editPaste;
+    public MenuItem editFind;
+
     @Override public void initialize(URL url, ResourceBundle resourceBundle) {
-        initViewOptions();
+        initViewEvents();
+        initEditEvents();
     }
 
-    private void initViewOptions() {
+    private void initEditEvents() {
+        editCut.setOnAction(event -> {
+            textArea.cut();
+        });
+        editCopy.setOnAction(event ->{
+            textArea.copy();
+        });
+        editPaste.setOnAction(event -> {
+            textArea.paste();
+        });
+        editFind.setOnAction(event -> {
+            TextInputDialog findTextDialog = new TextInputDialog();
+            findTextDialog.setTitle("Text Finder");
+            findTextDialog.setHeaderText("Enter text to find in the text area");
+            findTextDialog.showAndWait();
+            String result = findTextDialog.getResult();
+
+            ObservableList<CharSequence> paragraphs = textArea.getParagraphs();
+            int totalResultsFound = 0;
+            for (CharSequence paragraph : paragraphs) {
+                StringBuilder p = new StringBuilder(paragraph);
+                int fromIndex = 0;
+                while (fromIndex <= p.length() - 1) {
+                    int resultIndex = p.indexOf(result, fromIndex);
+                    if (resultIndex == -1) {
+                        break;
+                    } else {
+                        totalResultsFound++;
+                        fromIndex += resultIndex + 1;
+                    }
+                }
+            }
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information");
+            alert.setHeaderText(String.format("Total results found: %d", totalResultsFound));
+            alert.show();
+        });
+    }
+
+    private void initViewEvents() {
         showStatusBar.setOnAction(event -> {
             if (statusBar.isVisible()) {
                 statusBar.setVisible(false);
