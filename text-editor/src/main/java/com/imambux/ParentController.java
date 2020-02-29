c42fd7d649d10d19d71e115c0ba7ee255b4dc4eb
Imam Bux <imam.bux@idealo.de>
2020-02-29 10:35:51 +0100

    Control View item

--- text-editor/src/main/java/com/imambux/ParentController.java
+++ text-editor/src/main/java/com/imambux/ParentController.java
@@ -4,10 +4,26 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.HBox;
 
 public class ParentController implements Initializable {
 
-    @Override public void initialize(URL url, ResourceBundle resourceBundle) {
+    public MenuItem showStatusBar;
+    public HBox statusBar;
 
+    @Override public void initialize(URL url, ResourceBundle resourceBundle) {
+        initViewOptions();
+    }
+
+    private void initViewOptions() {
+        showStatusBar.setOnAction(event -> {
+            if (statusBar.isVisible()) {
+                statusBar.setVisible(false);
+            } else {
+                statusBar.setVisible(true);
+            }
+            statusBar.managedProperty().bind(statusBar.visibleProperty());
+        });
     }
 }
